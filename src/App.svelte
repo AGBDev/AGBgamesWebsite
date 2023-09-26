6796279737b8538ba0df8112f60408770a151268
AGBDev <nils19052006@gmail.com>
2023-09-26 16:17:11 +0200

    b

---
+++ src/App.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import WebsiteTitle from "./lib/WebsiteTitle.svelte";
+  import Blog from "./lib/blog/Blog.svelte";
+  import ButtonBar from "./lib/buttonbar/ButtonBar.svelte";
+</script>
+
+<main>
+  <WebsiteTitle title="DJ und Producer" visible={true} size={1} />
+  <WebsiteTitle
+    title="Deep House - Progressive House - Tech House"
+    visible={true}
+    size={1}
+  />
+
+  <div class="gfg">
+    <img src="robert.jpg" alt="" />
+    <div class="text-container">
+      <h1
+        style="font-family: 'Times New Roman', Times, serif; font-size:xx-large"
+      >
+        DJ Robert Lenz
+      </h1>
+      <h1
+        style="font-family: 'Times New Roman', Times, serif; font-size:x-large"
+      >
+        DJ und Producer
+      </h1>
+    </div>
+  </div>
+
+  <ButtonBar />
+  <br />
+  <br />
+  <WebsiteTitle title="News, Auftritte und Blog" visible={true} size={1} />
+
+  <Blog />
+</main>
+
+<style>
+  .gfg {
+    margin: 1%;
+    position: relative;
+  }
+
+  /*This CSS code is defining the styles for a container that holds text*/
+  .text-container {
+    position: absolute;
+    color: rgb(255, 255, 255);
+    left: 43rem;
+    top: 33rem;
+    /* background-color: rgb(41, 41, 41, 0.8); */
+    padding: 1 1rem;
+  }
+</style>
---
+++ src/lib/WebsiteTitle.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    export let title: string = "";
+    export let size: number = 1;
+    export let visible: boolean = false;
+
+    export const toggle = () => {
+        visible = !visible;
+    }
+</script>
+
+
+{#if visible}
+
+{#if size == 1}
+<h1>{title}</h1>
+{:else if size == 2}
+<h2>{title}</h2>
+{:else if size == 3}
+<h3>{title}</h3>
+{:else if size == 4}
+<h4>{title}</h4>
+{/if}
+
+{/if}
